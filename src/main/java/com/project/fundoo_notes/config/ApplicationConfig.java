c0e10a87259387a8973d7e9c861e62aecaaa42fe
swati1996 <swatirangari44@gmail.com>
2021-08-30 19:09:36 +0530

    Added: model mapper

---
+++ src/main/java/com/project/fundoo_notes/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.project.fundoo_notes.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
+    }
+}
