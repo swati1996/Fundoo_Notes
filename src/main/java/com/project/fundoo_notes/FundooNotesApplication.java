a81bbb04c3bb4b8c62056ef3591efdb462b9739d
swati1996 <swatirangari44@gmail.com>
2021-08-30 14:49:57 +0530

    Added: Swagger configure

--- src/main/java/com/project/fundoo_notes/FundooNotesApplication.java
+++ src/main/java/com/project/fundoo_notes/FundooNotesApplication.java
@@ -9,5 +9,4 @@ public class FundooNotesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FundooNotesApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/project/fundoo_notes/config/Swagger.java
@@ -0,0 +1,42 @@
+package com.project.fundoo_notes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class Swagger  {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.project.fundoo_notes"))
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot REST API",
+                "Spring Boot REST API for Online Store",
+                "1.0",
+                "Terms of service",
+                new Contact("Swati Rangari",
+                        "https://springframework.guru/about/",
+                        "swatirangari@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+        );
+        return apiInfo;
+    }
+}
