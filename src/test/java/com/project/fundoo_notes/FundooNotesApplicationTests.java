fa4117b3c4d6dc13a958f8b594c3cc7c1abd1ae5
swati1996 <swatirangari44@gmail.com>
2021-08-30 14:31:35 +0530

    Added: project setup

---
+++ src/test/java/com/project/fundoo_notes/FundooNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fundoo_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundooNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
