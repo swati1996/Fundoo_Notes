7ba62e7c3e3ed324223081dbc11b2ca3376ed258
swati1996 <swatirangari44@gmail.com>
2021-08-30 19:36:29 +0530

    Added: Responce dto

--- src/main/java/com/project/fundoo_notes/dto/ResponseDTO.java
+++ src/main/java/com/project/fundoo_notes/dto/ResponseDTO.java
@@ -1,6 +1,14 @@
 package com.project.fundoo_notes.dto;
 
 
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ResponseDTO {
     private String message;
     private int statuscode;
