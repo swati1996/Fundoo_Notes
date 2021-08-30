86f92bd5da4fd2e874e5ffcc44e5da62bd156f5e
swati1996 <swatirangari44@gmail.com>
2021-08-30 15:11:07 +0530

    Added: NoteModel Added

---
+++ src/main/java/com/project/fundoo_notes/model/NoteModel.java
@@ -0,0 +1,45 @@
+package com.project.fundoo_notes.model;
+
+import lombok.Data;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class NoteModel {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name="Title")
+    private String title;
+    @Column(name="NoteDescription")
+    private String description;
+    @Column(name="UserId")
+    private long userId;
+    @Column(name = "registeredDate")
+    private LocalDateTime registerDate;
+    @Column(name = "UpdatedDate")
+    private LocalDateTime updateDate;
+    @Column(name="trash")
+    private boolean trash;
+    @Column(name="archieve")
+    private boolean isArchieve;
+    @Column(name="pin")
+    private boolean pin;
+    @Column(name="labelid")
+    private Long labelId;
+    @Column(name="emailid")
+    private String emailid;
+    @Column(name="color")
+    private String color;
+    @Column(name="reminder")
+    private LocalDateTime remindertime;
+
+}
